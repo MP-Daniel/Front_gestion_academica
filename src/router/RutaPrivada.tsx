b098b315dc3c19ee3e680da4a92a7f98125e2d72
Daniel_Macias <damacias24@itp.edu.co>
2026-07-05 20:02:36 -0500

    Estructura de carpetas completa con comentario para su contenido

--- src/router/RutaPrivada.tsx
+++ src/router/RutaPrivada.tsx
@@ -1,8 +1,1 @@
-// router/RutaPrivada.tsx
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuthStore } from "@/store/auth.store";
-
-export function RutaPrivada() {
-  const token = useAuthStore((s) => s.token);
-  return token ? <Outlet /> : <Navigate to="/login" replace />;
-}
+// Protege rutas autenticadas
--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -1,10 +1,1 @@
 // Definición de tipos compartidos para autenticación y autorización.
-
-export type Rol = 'admin' | 'usuario';
-
-export interface Usuario {
-	id: number;
-	nombre: string;
-	email: string;
-	rol: Rol;
-}
